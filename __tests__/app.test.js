0955c621297e3e1b3b3919f6a2e9f8672f7c1be7
Emilija Malogajski <emalogajski@gmail.com>
2020-12-05 16:35:28 +0100

    Finishing up jest tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,19 +1,19 @@
 // import { removeTrip } from '../src/client/js/app';
 
+const { removeTrip } = require("../src/client/js/app");
+
 describe('removeTrip button', () => {
   test('Trip gets removed when removeTrip button is clicked', () => {
-    const tripList = document.getElementById('trips');
-    const removeButton = document.getElementById('removetrip');
-
     document.body.innerHTML = `<div id="managetrips">
     <button id="generate">Take me there</button>
     <button id="removetrip">Remove Last Added Trip</button>
-</div><div id="trips">
+    </div><div id="trips">
     <div class="trips"></div>
     </div>`;
 
-    removeButton.click();
-
-    expect(tripList.childNodes.length === 0).toBeTruthy();
+    const tripList = document.getElementById('trips');
+    const tripListLength = tripList.childNodes.length;
+    removeTrip();
+    expect(tripListLength - tripList.childNodes.length === 1).toBeTruthy();
   });
 });
